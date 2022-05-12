e29846944de8e42280290fc389c4caa433ee4541
wufan08 <wufan08@corp.netease.com>
2022-05-12 21:02:35 +0800

    feat: 初æ创建项目

---
+++ .eslintrc.js
@@ -0,0 +1,54 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true
+  },
+  parser: 'vue-eslint-parser',
+  extends: [
+    'plugin:@typescript-eslint/recommended',
+    'plugin:vue/vue3-recommended',
+    'airbnb-base'
+  ],
+  parserOptions: {
+    ecmaVersion: 'latest',
+    parser: '@typescript-eslint/parser',
+    sourceType: 'module'
+  },
+  plugins: [
+    'vue',
+    '@typescript-eslint'
+  ],
+  rules: {
+    indent: ['error', 2],
+    semi: ['error', 'never'],
+    'comma-dangle': ['error', 'never'],
+    'standard/no-callback-literal': 'off',
+    '@typescript-eslint/ban-types': 'off',
+    '@typescript-eslint/explicit-module-boundary-types': 'off',
+    '@typescript-eslint/member-delimiter-style': ['error',
+      {
+        multiline: {
+          delimiter: 'none'
+        },
+        singleline: {
+          delimiter: 'comma'
+        }
+      }],
+    '@typescript-eslint/no-explicit-any': 'off',
+    'space-before-function-paren': ['error', 'never'],
+    'vue/html-self-closing': 'off',
+    'vue/array-bracket-spacing': 'error',
+    'vue/arrow-spacing': 'error',
+    'vue/block-spacing': 'error',
+    'vue/brace-style': 'error',
+    'vue/camelcase': 'error',
+    'vue/comma-dangle': 'error',
+    'vue/component-name-in-template-casing': 'error',
+    'vue/eqeqeq': 'error',
+    'vue/key-spacing': 'error',
+    'vue/match-component-file-name': 'error',
+    'vue/no-v-html': 'off',
+    'vue/object-curly-spacing': 'error',
+    'import/no-extraneous-dependencies': ['error', { devDependencies: true }]
+  }
+}

689294f5b9cf001f2d1c1936053f6bf36f0d609a
wufan08 <wufan08@corp.netease.com>
2022-05-20 22:33:20 +0800

    fix: 更换链接字段

--- src/views/home/Index.vue
+++ src/views/home/Index.vue
@@ -5,15 +5,17 @@ import { getDailyPic } from '../../utils/api'
 
 const url = ref<string>('')
 handle(getDailyPic(), '获取每日图片', false, true).then((res: any) => {
-  url.value = res.data.url
+  url.value = res.data.src
 })
 </script>
 
 <template>
   <div class="home-index">
     <img
+      v-if="url"
       :src="url"
       alt="每日图片"
+      class="home-img"
     >
   </div>
 </template>

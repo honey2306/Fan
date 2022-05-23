341995992263cb6f5835c3066175749fbb12c7d5
wufan08 <wufan08@corp.netease.com>
2022-05-23 23:57:54 +0800

    feat: 首页布局

--- src/views/home/Index.vue
+++ src/views/home/Index.vue
@@ -1,11 +1,32 @@
 <script lang="ts" setup>
-import Daily from './daily.vue'; </script>
+import { moduleConfig } from './moduleConfig'
+
+const itemLength = 5
+
+console.log(moduleConfig[0].name)
+
+</script>
 
 <template>
   <div class="home-index">
-    <daily />
-    <div class="logo">
-      Fan
+    <div
+      v-for="item in itemLength"
+      :key="item"
+      class="main-wrap"
+    >
+      <div
+        v-if="item !== 1"
+        class="item"
+      >
+        <div class="img"></div>
+        <div class="title">
+          {{ moduleConfig[item - 2].label }}
+        </div>
+      </div>
+      <div
+        v-else
+        class="menu"
+      ></div>
     </div>
   </div>
 </template>
---
+++ src/views/home/moduleConfig.ts
@@ -0,0 +1,13 @@
+export const moduleConfig: Array<any> = [{
+  name: 'blog',
+  label: '文章'
+}, {
+  name: 'life',
+  label: '生活'
+}, {
+  name: 'travel',
+  label: '足迹'
+}, {
+  name: 'archive',
+  label: '归档'
+}]
